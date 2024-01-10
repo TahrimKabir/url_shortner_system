62c9ad6016ac5ad91e98d64c7212c4f92f4c2e1c
tahrim <tahrimkabir321@gmail.com>
2024-01-10 23:46:22 +0600

    url migration

---
+++ url-shortner/database/migrations/2024_01_10_173114_url.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Url extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('url', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('long_url');
+            $table->string('short_url');
+            $table->integer('click');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
